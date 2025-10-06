6b220af0a15bc90e6a4fdcb616d7555fb82886b7
mehrozezaidi <mehrozabbas14@gmail.com>
2025-10-06 22:12:06 +0500

    Deploy update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1714892832"
+    serviceWorkerVersion: "1193812590"
   }
 });
